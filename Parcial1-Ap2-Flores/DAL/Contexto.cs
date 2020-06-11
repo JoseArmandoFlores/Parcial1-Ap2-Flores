48649935f4236988d434a06f1982e4796b757c85
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-06-10 20:30:06 -0400

    Creando el contexto

--- Parcial1-Ap2-Flores/DAL/Contexto.cs
+++ Parcial1-Ap2-Flores/DAL/Contexto.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Parcial1_Ap2_Flores.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,9 +9,10 @@ namespace Parcial1_Ap2_Flores.DAL
 {
     public class Contexto:DbContext
     {
+        public DbSet<Articulos> Articulos { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlite(@"Data Source = DATA\Nombre.db");
+            optionsBuilder.UseSqlite(@"Data Source = DATA\Articulos.db");
         }
     }
 }
