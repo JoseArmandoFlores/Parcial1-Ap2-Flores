bbf52d321995d082f2087a18fdb48267bc50b048
Jose Armando Flores Baldera <jose_flores3@ucne.edu.do>
2020-06-10 20:46:07 -0400

    Creando la migracion

---
+++ Parcial1-Ap2-Flores/Migrations/20200611004534_Migracion_Inicial.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Parcial1_Ap2_Flores.Migrations
+{
+    public partial class Migracion_Inicial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    ProductoId = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(nullable: false),
+                    Existencia = table.Column<int>(nullable: false),
+                    Costo = table.Column<decimal>(nullable: false),
+                    ValorInvetario = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.ProductoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
